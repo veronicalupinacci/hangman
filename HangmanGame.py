1d8ee5002f4d1bb2fd9a2655e0b7bdb2c879dfae
Veronica L <vlupinacci02@gmail.com>
2023-06-27 08:04:28 -0400

    Revert "Accept only char an # of wrong entries fixed"
    This reverts commit f2b062ba79a0d6d79e6a0a27066cfadc7071d951.

--- HangmanGame.py
+++ HangmanGame.py
@@ -5,78 +5,50 @@ has to guess letters of the word
 and then gets one chance to guess the word.
 """
 
-__author__ = "Veronica Lupinacci"
-__version__ = "6/27/2023"
+__author__ = "Veronica"
+__version__ = "6/26/2023"
 
 import random
 
-def only_char(x): #function to check if the input is a character only, part of the alphabet and change it to lowercase
-    
-    while x.isalpha() != True or len(x) > 1:
-        print ("Please enter only one character, it has to be a letter.")
-        x = input()
-
-    return x.lower()
-
-words = ["hello", "morning", "day", "toy", "night", "baby", "sun", "bedroom", "pencil", "happy"] #list of possdible words
+words = ["hello", "morning", "day", "toy", "night", "baby", "sun", "bedroom", "pencil", "highlighter"] #list of possdible words
 chosen = random.choice(words) #pick a random word
 
 guessed = "" #string for right letters
 wrong = "" #string for wrong letters
 wordsinput = "" #string for all letters already input
 rightcount = 0 #counter for guessed letters
-tries = 0 #counter for tries, counts only wrong entries
 
 
 print("The word has", len(chosen), "letters") #tell the user how many characters has the word
 
 
-while rightcount < len(chosen) and tries <= 4 : #while loop until all letters in the word are guessed
+while rightcount < len(chosen): #while loop until all letters in the word are guessed
     print("Guess a letter") #get user input
     x = input()
-    checked_input = only_char(x) #save in a new variable the result of the only_char function
-
-    wordsinput = wordsinput + checked_input + " " #add word to input string
-
-    if checked_input in chosen: #check if the user guessed right
+    
+    if x in chosen: #check if the user guessed right
         print ("Good job! You guessed right.\n")
-        guessed += checked_input + " "#add word to guessed string
+        guessed += x + " "#add word to guessed string
+        wordsinput+= x + " " #add word to input string
         rightcount+=1 #increment counter
+        print ("Letters guessed so far:", wordsinput, "\nRight:", guessed, "   Wrong:", wrong, "\n") #output message to tell the user guessed, wrong and all input letters 
 
     else: #if the user is wrong
         print ("Oh no! wrong guess.\n")
-        tries += 1 #increment tries counter
-        wrong += checked_input + " "#add word to wrong string
+        wrong += x + " "#add word to wrong string
 
-    print ("Letters guessed so far:", wordsinput, "\nRight:", guessed, "   Wrong:", wrong, "\n") #output message to tell the user guessed, wrong and allinput letters
+        print ("Letters guessed so far:", wordsinput, "\nRight:", guessed, "   Wrong:", wrong, "\n") #output message to tell the user guessed, wrong and all input letters 
 
-if tries > 4 : #check if the while loop was exited because the user lost 
-    print("GAME OVER \nToo many tries, the word was:", chosen)
-else: #else, continue the game
-    tries = 0 #reset tries counter
-    check = False #variable to check if the answer is right or wrong
+   
+#output message to tell the user that all words were guessed and what are those.
+#Then tell the user to guess the full word
+print ("All letters of the letters are guessed! \nLetters:", guessed, "\n\nNow guess the word (NOTE the letters could appear more than once) : ")
+x = input()
 
-    #output message to tell the user that all words were guessed and what are those.
-    #Then tell the user to guess the full word
-    print ("All letters of the word are guessed! \nLetters:", guessed, "\n\nNow guess the word (NOTE the letters could appear more than once) : ")
-    x = input()
-        
-    while check == False and tries <= 3 : #start while lop
-        if x == chosen: #check if input equals to chosen word
-            check = True #set check variable to True
-            
-        else: #if guessed wrong, tell the user they lost
-            check = False
-            tries += 1 #increment tries counter
-            print ("Oh no! Guess again")
-            x = input()
-    #check if user won or lost
-    if check == True: 
-        print("YOU WON!")
-    else:
-        print("GAME OVER.\nToo many tries, the word was:", chosen)
+if x == chosen: #if guessed right, tell the user they won
+    print("You won!")
+    
+else: #if guessed wrong, tell the user they lost
+    print ("Oh no! wrong guess. Game over.")
 
-#end message
-print("Thank you for playing!")
-
-        
+    
